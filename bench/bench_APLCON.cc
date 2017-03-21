69812044b83beca2951473a6ff13db50805a74b2
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-21 23:09:18 +0100

    Bench: Add linefit (no fixed x yet)

--- bench/bench_APLCON.cc
+++ bench/bench_APLCON.cc
@@ -6,7 +6,10 @@
 #include <ostream>
 #include <vector>
 #include <array>
+#include <algorithm>
+#include <iostream>
 
+using namespace std;
 
 struct X {
 
@@ -22,6 +25,10 @@ struct X {
 
     friend std::ostream& operator<<(std::ostream& s, const X& o) {
         return s << "(" << o.Value << "," << o.Sigma << ")";
+    }
+
+    operator double() const {
+        return Value;
     }
 };
 
@@ -42,6 +49,36 @@ static void BM_ErrorPropagation(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_ErrorPropagation);
+
+static void BM_LineFit(benchmark::State& state) {
+
+    auto residuals = [] (const X& a, const X& b, const vector<X>& x, const vector<X>& y) {
+
+        // we use again std::transform to calculate the residuals
+        vector<double> residuals(y.size());
+        transform(x.begin(), x.end(), y.begin(), residuals.begin(),
+                  [&a, &b] (const double& x_i, const double& y_i) {
+            return a + b*x_i - y_i;
+        });
+        return residuals;
+    };
+
+    while (state.KeepRunning()) {
+
+        vector<X> x{ {1,   0.2}, {2,   0.23}, {3,   0.16}, {4,   0.21} };
+        vector<X> y{ {1.1,0.08}, {1.95,0.04}, {2.02,0.11}, {3.98,0.07} };
+
+        // two unmeasured variables
+        X a{0,0};
+        X b{0,0};
+
+        APLCON::Fitter<X, X, vector<X>, vector<X>> fitter;
+        fitter.DoFit(a, b, x, y, residuals);
+    }
+}
+
+BENCHMARK(BM_LineFit);
+
 
 BENCHMARK_MAIN()
 
