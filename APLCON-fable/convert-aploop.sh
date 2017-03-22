553ff9630cb3f2391aa41680abb4ea4a680be850
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-23 00:14:24 +0100

    Only build generated fable source

--- APLCON-fable/convert-aploop.sh
+++ APLCON-fable/convert-aploop.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-gfortran -w -c aploop.F -save-temps -o /dev/null
-rm aploop.s
-fable.cout aploop.f90 > aploop.cpp
-rm aploop.f90
+#!/bin/bash
+function make_f90 {
+    gfortran -w -c $1.F -save-temps -o /dev/null
+    rm $1.s
+}
+LIST="condutil.f90 aploop.f90 chprob.f90"
+for i in $LIST; do
+    make_f90 ${i%.*}
+done
+fable.cout --namespace aplcon_fortran $LIST > APLCON-fable.cpp
+rm $LIST
