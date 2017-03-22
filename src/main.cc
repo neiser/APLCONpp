ec932c250c18d52f7722e4cc4aec5a6aa0f5a168
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-22 02:50:20 +0100

    Add simple main for testing

---
+++ src/main.cc
@@ -0,0 +1,50 @@
+
+#include "APLCON.hpp"
+
+#include <ostream>
+
+using namespace std;
+
+struct X {
+
+    constexpr X(double v, double s) : Value(v), Sigma(s) {}
+
+    double Value;
+    double Sigma;
+
+    template<size_t N>
+    std::tuple<double&> linkFitter() noexcept {
+//        return N == APLCON::ValueIdx ? std::tie(Value,Value) : std::tie(Sigma,Sigma);
+        return N == APLCON::ValueIdx ? Value : Sigma;
+    }
+
+    template<size_t N>
+    APLCON::Variable_Settings_t getFitterSettings() const noexcept {
+        APLCON::Variable_Settings_t settings;
+        settings.StepSize = N;
+        return settings;
+    }
+
+    friend std::ostream& operator<<(std::ostream& s, const X& o) {
+        return s << "(" << o.Value << "," << o.Sigma << ")";
+    }
+
+    operator double() const {
+        return Value;
+    }
+};
+
+int main() {
+    auto a_and_b_is_c = [] (const X& a, const X& b, const X& c) {
+        return c.Value - a.Value - b.Value;
+    };
+
+    X a{10, 0.3};
+    X b{20, 0.4};
+    X c{ 0, 0.0};
+
+    APLCON::Fitter<X, X, X> fitter;
+    fitter.DoFit(a, b, c, a_and_b_is_c);
+
+
+}
