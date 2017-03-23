eb66dac95f0033808aae46ed0e4d27825d631035
Andreas Neiser <neiser@kph.uni-mainz.de>
2017-03-23 13:36:27 +0100

    Add getters for Value/Sigma/Pull, little comments

--- test/Value_t.h
+++ test/Value_t.h
@@ -1,22 +1,26 @@
 #pragma once
 
+#include "APLCON.hpp"
+
 struct Value_t {
 
     constexpr Value_t(double v, double s) : V_S_P{v, s, 0}{}
 
-    std::tuple<double,double,double> V_S_P;
+    std::tuple<double,double,double> V_S_P; // short for Value, Sigma, Pull
 
     bool Fixed = false;
     bool Poisson = false;
 
+    // fitter is able to access fields to fit them (last one pull is just written, actually)
     template<size_t N>
     std::tuple<double&> linkFitter() noexcept {
         return std::tie(std::get<N>(V_S_P));
     }
 
+    // user can provide settings for each variabl
     template<size_t innerIdx>
     APLCON::Variable_Settings_t getFitterSettings(size_t outerIdx) const noexcept {
-        (void)outerIdx; // unused, provided to user struct for completeness
+        (void)outerIdx; // unused, provided to user method for completeness
         APLCON::Variable_Settings_t settings;
         if(Fixed)
             settings.StepSize = 0;
@@ -25,11 +29,22 @@ struct Value_t {
         return settings;
     }
 
+    // the following methods are just user convenience (not required by fitter)
+
     friend std::ostream& operator<<(std::ostream& s, const Value_t& o) {
-        return s << "(" << std::get<APLCON::ValueIdx>(o.V_S_P)
-                 << "," << std::get<APLCON::SigmaIdx>(o.V_S_P)
-                 << "," << std::get<APLCON::PullIdx>(o.V_S_P)
-                 << ")";
+        return s << "(" << o.Value() << "," << o.Sigma() << "," << o.Pull() << ")";
+    }
+
+    double Value() const {
+        return std::get<APLCON::ValueIdx>(V_S_P);
+    }
+
+    double Sigma() const {
+        return std::get<APLCON::SigmaIdx>(V_S_P);
+    }
+
+    double Pull() const {
+        return std::get<APLCON::PullIdx>(V_S_P);
     }
 
     operator double() const {
