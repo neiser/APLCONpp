192b934d65fd96fef8956c78d81c8d7868c12c55
Andreas Neiser <andreas.neiser@gmail.com>
2018-07-12 23:02:16 +0200

    Add converted C code with f2c, compiles cleanly

---
+++ APLCON/chprob.f90.c
@@ -0,0 +1,137 @@
+/* chprob.f90.F -- translated by f2c (version 12.02.01).
+   You must link the resulting object file with libf2c:
+	on Microsoft Windows system, link with libf2c.lib;
+	on Linux or Unix systems, link with .../path/to/libf2c.a -lm
+	or, if you install libf2c.a in a standard place, with -lf2c -lm
+	-- in that order, at the end of the command line, as in
+		cc *.o -lf2c -lm
+	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
+
+		http://www.netlib.org/f2c/libf2c.zip
+*/
+
+#include <stdlib.h> /* For exit() */
+#include <f2c.h>
+
+doublereal chprob_(doublereal *chisq, integer *n)
+{
+    /* System generated locals */
+    doublereal ret_val, d__1, d__2;
+
+    /* Local variables */
+    extern doublereal dgamin_(doublereal *, doublereal *);
+
+/*     chi square probability for N degrees of freedom at CHISQ */
+/*     =integral from 0 ... CHISQ */
+/* prob from chisquare */
+/*     ... */
+    if (*chisq <= 0.f) {
+	ret_val = 1.;
+    } else {
+	d__1 = *n * .5;
+	d__2 = *chisq * .5;
+	ret_val = 1. - dgamin_(&d__1, &d__2);
+    }
+    return ret_val;
+} /* chprob_ */
+
+doublereal dgamin_(doublereal *a, doublereal *x)
+{
+    /* System generated locals */
+    doublereal ret_val, d__1;
+
+    /* Local variables */
+    static doublereal b, c__, d__, h__;
+    static integer i__, n;
+    static doublereal an, ap, del, gln, sum;
+    extern doublereal dgamml_(doublereal *);
+
+/*     incomplete gamma function P(a,x) */
+/*     returns -1.0 for x < 0 or a =< 0 */
+/*     ... */
+    if (*x < 0. || *a <= 0.) {
+	ret_val = -1.;
+/* error */
+    } else if (*x == 0.f) {
+	ret_val = 0.;
+    } else if (*x < *a + 1.) {
+/* series representation */
+	gln = dgamml_(a);
+/* ln[Gamma(a)] */
+	ap = *a;
+	sum = 1. / *a;
+	del = sum;
+	for (n = 1; n <= 300; ++n) {
+	    ap += 1.f;
+	    del = del * *x / ap;
+	    sum += del;
+	    if (abs(del) < abs(sum) * 3e-7) {
+		goto L10;
+	    }
+	}
+	s_stop("DGAMIN:  error 1", (ftnlen)16);
+L10:
+	ret_val = sum * exp(-(*x) + *a * log(*x) - gln);
+    } else {
+/* continued fraction representation */
+	gln = dgamml_(a);
+/* ln[Gamma(a)] */
+	b = *x + 1. - *a;
+	c__ = 9.9999999999999988e29;
+	d__ = 1. / b;
+	h__ = d__;
+	for (i__ = 1; i__ <= 300; ++i__) {
+	    an = -i__ * (i__ - *a);
+	    b += 2.;
+	    d__ = an * d__ + b;
+	    if (abs(d__) < 1e-30) {
+		d__ = 1e-30;
+	    }
+	    c__ = b + an / c__;
+	    if (abs(c__) < 1e-30) {
+		c__ = 1e-30;
+	    }
+	    d__ = 1. / d__;
+	    del = d__ * c__;
+	    h__ *= del;
+	    if ((d__1 = del - 1., abs(d__1)) < 3e-7) {
+		goto L20;
+	    }
+	}
+	return ret_val;
+L20:
+	ret_val = 1. - exp(-(*x) + *a * log(*x) - gln) * h__;
+    }
+    return ret_val;
+} /* dgamin_ */
+
+doublereal dgamml_(doublereal *x)
+{
+    /* Initialized data */
+
+    static doublereal cof[6] = { 76.18009172947146,-86.50532032941677,
+	    24.01409824083091,-1.231739572450155,.001208650973866179,
+	    -5.395239384953e-7 };
+    static doublereal stp = 2.5066282746310005;
+
+    /* System generated locals */
+    doublereal ret_val;
+
+    /* Local variables */
+    static integer j;
+    static doublereal xx, yy, ser, tmp;
+
+/* ln[Gamma(x)] */
+/*     ... */
+    xx = *x;
+    yy = xx;
+    tmp = (xx + .5) * log(xx + 5.5) - xx - 5.5;
+    ser = 1.000000000190015;
+    for (j = 1; j <= 6; ++j) {
+	yy += 1.;
+	ser += cof[j - 1] / yy;
+    }
+    ret_val = tmp + log(stp * ser / xx);
+    return ret_val;
+} /* dgamml_ */
+
---
+++ APLCON/condutil.f90.c
@@ -0,0 +1,383 @@
+/* condutil.f90.F -- translated by f2c (version 12.02.01).
+   You must link the resulting object file with libf2c:
+	on Microsoft Windows system, link with libf2c.lib;
+	on Linux or Unix systems, link with .../path/to/libf2c.a -lm
+	or, if you install libf2c.a in a standard place, with -lf2c -lm
+	-- in that order, at the end of the command line, as in
+		cc *.o -lf2c -lm
+	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
+
+		http://www.netlib.org/f2c/libf2c.zip
+*/
+
+#include <stdlib.h> /* For exit() */
+#include <f2c.h>
+
+integer ijsym_(integer *i__, integer *j)
+{
+    /* System generated locals */
+    integer ret_val;
+
+/* index (I,J)=(J,I) in symmetric matri */
+/* ,NCOUNT */
+    if (*i__ <= *j) {
+	ret_val = (*j * *j - *j) / 2 + *i__;
+    } else {
+	ret_val = (*i__ * *i__ - *i__) / 2 + *j;
+    }
+    return ret_val;
+} /* ijsym_ */
+
+/* Subroutine */ int duminv_(doublereal *a, doublereal *w, doublereal *b, 
+	integer *nx, integer *nf, integer *mb, integer *nrank, doublereal *
+	aux, doublereal *qnext)
+{
+    /* Initialized data */
+
+    static doublereal eps = 1e-6;
+
+    /* System generated locals */
+    integer i__1, i__2, i__3;
+    doublereal d__1, d__2, d__3;
+
+    /* Local variables */
+    static integer i__, j, k, l, m, n, ia, ij, jk, jj, kk, jl, lk;
+    static doublereal vjk, vkk, sum;
+    static integer last, nmeas, jlast;
+    static logical solve;
+    static integer jfirst;
+
+/*     Obtain solution of a system of linear equations V *  X  =  B  with */
+/*     symmetric matrix V and inverse (for M =  1)  or  matrix  inversion */
+/*     only (for M = 0). */
+
+/*                   - - - - */
+/*        CALL SMINV(W,B,N,M,NRANK,AUX,QNEXT) */
+/*                   - -     ----- */
+
+/*           W = symmetric N-by-N matrix in symmetric storage mode */
+/*               W(1) = W11, W(2) = W12, W(3) = W22, W(4) = W13, . . . */
+/*               replaced by inverse matrix */
+/*           B = N-vector   (for M = 0 use a dummy argument) */
+/*               replaced by solution vector */
+/*           M = see above */
+
+
+/*     Method of solution is by elimination selecting the  pivot  on  the */
+/*     diagonal each stage. The rank of the matrix is returned in  NRANK. */
+/*     For NRANK ne N, all remaining  rows  and  cols  of  the  resulting */
+/*     matrix V and the corresponding elements of  B  are  set  to  zero. */
+/*     SMINV can be used for a dimension up to 100 (see INVCDR). */
+
+/* matrix inve */
+    /* Parameter adjustments */
+    --qnext;
+    --aux;
+    --b;
+    --w;
+    --a;
+
+    /* Function Body */
+/*     special entry for partial inversion ****************************** */
+/*     ... */
+    *nrank = 0;
+    n = *nx + *nf;
+/*     make sure AUX is zero, prevents uninit access */
+/*     in continued execution of DBMINV */
+/* dimension parameter */
+    i__1 = n;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+	aux[i__] = 0.;
+    }
+/*     -VX(NX-sym) is already inserted in W(NX+NF-sym) ------------------ */
+    ij = (*nx * *nx + *nx) / 2;
+/* number of elements of V(.) */
+    i__1 = n;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+	qnext[i__] = 0.;
+/* reset pointer */
+    }
+    ia = 0;
+    i__1 = *nf;
+    for (j = 1; j <= i__1; ++j) {
+	i__2 = *nx;
+	for (i__ = 1; i__ <= i__2; ++i__) {
+	    w[ij + i__] = a[ia + i__];
+/* copy A(.) into W_12 */
+	}
+	i__2 = j;
+	for (i__ = 1; i__ <= i__2; ++i__) {
+	    w[ij + *nx + i__] = 0.f;
+/* reset last submatrix W_22 of W(.) */
+	}
+	ij = ij + *nx + j;
+	ia += *nx;
+    }
+/*     distinguish between measured and unmeasured variables ------------ */
+    jfirst = 0;
+/* first index of measured variable */
+    nmeas = 0;
+/* number of measured variables */
+    i__1 = *nx;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+	if (w[(i__ * i__ + i__) / 2] < 0.f) {
+/* measured variable */
+	    if (jfirst == 0) {
+		jfirst = i__;
+/* first index of measured variable */
+	    } else {
+		qnext[jlast] = (doublereal) i__;
+/* insert index at previous index */
+	    }
+	    jlast = i__;
+/* save index */
+	    ++nmeas;
+	}
+    }
+    if (jlast == 0) {
+	goto L10;
+    }
+/* nothing to do */
+    qnext[jlast] = -1.;
+/*     apply exchange algorithm to sub-matrices ------------------------- */
+/* stop index for last measured variable */
+    i__1 = n;
+    for (i__ = *nx + 1; i__ <= i__1; ++i__) {
+/* loop I over constraint equations */
+	j = jfirst;
+/* first index of unmeasured variable */
+	i__2 = nmeas;
+	for (m = 1; m <= i__2; ++m) {
+/* already inverted element index J */
+	    sum = 0.;
+	    jk = (j * j - j) / 2;
+/* index of diagonal element before */
+	    i__3 = *nx;
+	    for (k = 1; k <= i__3; ++k) {
+		if (k <= j) {
+		    ++jk;
+		}
+/* index in j column */
+		if (qnext[k] != 0.) {
+		    sum += w[jk] * w[(i__ * i__ - i__) / 2 + k];
+		}
+		if (k >= j) {
+		    jk += k;
+		}
+/* index in j row */
+	    }
+	    aux[j] = sum;
+/* = A-row * VX-row/col */
+	    j = (integer) qnext[j];
+/* next index of unmeasured variable */
+	}
+	i__2 = n;
+	for (k = i__; k <= i__2; ++k) {
+	    sum = 0.;
+	    j = jfirst;
+/* first index of unmeasured variable */
+	    i__3 = nmeas;
+	    for (m = 1; m <= i__3; ++m) {
+/* already inverted element index J */
+		sum += w[(k * k - k) / 2 + j] * aux[j];
+/* = A-row * H */
+		j = (integer) qnext[j];
+/* next index of unmeasured variable */
+	    }
+	    w[(k * k - k) / 2 + i__] += sum;
+/* add to diagonal W_22 */
+	}
+	j = jfirst;
+/* first index of unmeasured variable */
+	i__2 = nmeas;
+	for (m = 1; m <= i__2; ++m) {
+	    w[(i__ * i__ - i__) / 2 + j] = -aux[j];
+/* add to off-diagonal W_22 */
+	    j = (integer) qnext[j];
+/* next index of unmeasured variable */
+	}
+    }
+/*     set pointer for unmeasured variables ----------------------------- */
+    jfirst = 0;
+    jlast = 0;
+    i__1 = n;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+	if (qnext[i__] == 0.) {
+/* unmeasured variable */
+	    if (jfirst == 0) {
+		jfirst = i__;
+/* first index of unmeasured variable */
+	    } else {
+		qnext[jlast] = (doublereal) i__;
+/* next index of unmeasured variable */
+	    }
+	    jlast = i__;
+	} else {
+	    qnext[i__] = 0.;
+/* reset index for measured variable */
+	}
+    }
+    if (jlast == 0) {
+	goto L10;
+    }
+/* no unmeasured variable */
+    qnext[jlast] = -1.;
+/*     common code for inversion and (M=1) solution of matrix equation */
+/* end flag */
+L10:
+    solve = TRUE_;
+    if (*mb == 0) {
+	solve = FALSE_;
+    }
+/*     loop begin (loop on all remaining rows/cols) */
+/* solution flag */
+    i__1 = n;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+/* loop on all remaining elements */
+	vkk = 0.;
+/* search for pivot element */
+	k = 0;
+/* pivot index */
+	j = jfirst;
+/* first candidate index */
+	last = 0;
+L20:
+	if (j > 0) {
+/* test for linearity and zero matrix */
+	    jj = (j * j + j) / 2;
+/* diagonal index */
+/* Computing MAX */
+	    d__2 = abs(vkk), d__3 = eps * aux[j];
+	    if ((d__1 = w[jj], abs(d__1)) > max(d__2,d__3)) {
+		vkk = w[jj];
+/* largest pivot candidate so far */
+		k = j;
+/* index of largest */
+		l = last;
+	    }
+	    last = j;
+	    j = (integer) qnext[j];
+/* index of next candidate */
+	    goto L20;
+	}
+	if (k != 0) {
+/* pivot element found - proceed */
+	    ++(*nrank);
+/* increase rank counter */
+	    kk = (k * k + k) / 2;
+	    if (l == 0) {
+		jfirst = (integer) qnext[k];
+/* new first index */
+	    } else {
+		qnext[l] = qnext[k];
+/* bridge used index */
+	    }
+	    qnext[k] = 0.;
+/* reset used index */
+	    ++(*nrank);
+/* increase rank */
+	    vkk = 1.f / vkk;
+/* invert pivot */
+	    w[kk] = -vkk;
+	    if (solve) {
+		b[k] *= vkk;
+	    }
+	    jk = kk - k;
+	    jl = 0;
+	    i__2 = n;
+	    for (j = 1; j <= i__2; ++j) {
+/* elimination */
+		if (j == k) {
+		    jk = kk;
+		    jl += j;
+		} else {
+		    if (j < k) {
+			++jk;
+		    } else {
+			jk = jk + j - 1;
+		    }
+		    vjk = w[jk];
+		    w[jk] = vkk * vjk;
+		    if (solve) {
+			b[j] -= b[k] * vjk;
+		    }
+		    lk = kk - k;
+		    i__3 = j;
+		    for (l = 1; l <= i__3; ++l) {
+			++jl;
+			if (l == k) {
+			    lk = kk;
+			} else {
+			    if (l < k) {
+				++lk;
+			    } else {
+				lk = lk + l - 1;
+			    }
+			    w[jl] -= w[lk] * vjk;
+			}
+		    }
+		}
+	    }
+	} else {
+/* no pivot candadate found - reset */
+	    i__2 = n;
+	    for (k = 1; k <= i__2; ++k) {
+		if (qnext[k] != 0.) {
+/* undefined variable */
+		    if (solve) {
+			b[k] = 0.;
+		    }
+/* clear undefined vector element */
+		    i__3 = k;
+		    for (j = 1; j <= i__3; ++j) {
+			if (qnext[j] != 0.) {
+			    w[(k * k - k) / 2 + j] = 0.;
+			}
+/* clear matri */
+		    }
+		}
+	    }
+	    goto L30;
+	}
+    }
+/* end of inversion loop */
+L30:
+    i__1 = (n * n + n) / 2;
+    for (i__ = 1; i__ <= i__1; ++i__) {
+	w[i__] = -w[i__];
+/* finally reverse sign */
+    }
+    return 0;
+} /* duminv_ */
+
+doublereal scalxy_(doublereal *x, doublereal *y, integer *n)
+{
+    /* System generated locals */
+    integer i__1;
+    doublereal ret_val;
+
+    /* Local variables */
+    static integer j;
+    static doublereal sum;
+
+/*     Scalar product of two vectors */
+/*                - - -                 T */
+/*        S = VXY(X,Y,N)           S = X  * Y (scalar product) */
+
+/* scalar vector product */
+/* I,M */
+/*     ... */
+    /* Parameter adjustments */
+    --y;
+    --x;
+
+    /* Function Body */
+    sum = 0.;
+    i__1 = *n;
+    for (j = 1; j <= i__1; ++j) {
+	sum += x[j] * y[j];
+    }
+    ret_val = sum;
+    return ret_val;
+} /* scalxy_ */
+
