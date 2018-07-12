bc3a44648779b641ab59319bdde6a44bdd8ef9ad
Andreas Neiser <andreas.neiser@gmail.com>
2018-07-12 22:37:48 +0200

    Use f2c instead of fable in convert.sh

--- APLCON/convert.sh
+++ APLCON/convert.sh
@@ -3,5 +3,8 @@ gfortran -c condutil.F -save-temps
 gfortran -c aploop.F -save-temps
 gfortran -c chprob.F -save-temps
 rm *.s *.o
-fable.cout aploop.f90 chprob.f90 condutil.f90 --namespace aplcon > aploop.cpp
-rm *.f90
+mv condutil.f90 condutil.f90.F
+mv aploop.f90 aploop.f90.F
+mv chprob.f90 chprob.f90.F
+f2c *.f90.F
+rm *.f90.F
